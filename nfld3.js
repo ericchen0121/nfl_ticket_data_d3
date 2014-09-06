b8b560e379b8053172d291660f1d2960502cbe47
Eric Chen <eric.chen0121@gmail.com>
2014-09-06 01:50:24 -0700

    update data and transition off stage d3

--- nfld3.js
+++ nfld3.js
@@ -1,35 +1,47 @@
 $(function() {
-  width = 1000
-  height = 1000
+  width = 3000
+  height = 2000
   xoffset = 100
   yoffset = 100
   var items
 
-  function getData() {
-      $.getJSON('nfl_ticket_data.json', function(data) {
-      items = data
-      draw()
-    })
-  }
-
-  function draw() {
-      var svg = d3.select('body').append('svg')
+  // attach base svg element
+  var svg = d3.select('body').append('svg')
                         .attr('width', width)
                         .attr('height', height)
 
-      console.log(items)
-      var circles = svg.selectAll('circle')
-                    .data(items)
-                    .enter()
-                    .append('circle')
+  // the main draw function
+  function draw(data) {
 
-      var circleAttrs = circles
-                        .attr('cx', function(d, i){ return i * 20 + xoffset})
+
+      var circles = svg.selectAll('circle').data(data)
+
+      var circlesEnter = circles.enter().append('circle')
+
+      var circlesExit = circles.exit()
+
+      circlesExit.transition().duration(500).attr('r', 0).transition().duration(500).attr('opacity', 0)
+
+      var circleAttrs = circlesEnter
+                        .attr('r', 0)
+                        .transition().duration(1500)
+                        .attr('cx', function(d, i){ return i * 70 + xoffset})
                         .attr('cy', yoffset)
-                        .attr('r', function(d){ return d.avg_ticket / 5 })
+                        .attr('r', function(d){ return d.avg_ticket / 4 })
                         .style('fill', 'black')
   }
 
+  // get initial set of data and render it
+  $.getJSON('nfl_ticket_data.json', function(data) {
+    items = data
+    draw(items)
+    setInterval(function() {
+      items = items.splice(1, items.length -1)
+      // console.log(items)
+      draw(items)
+    }, 800)
+  })
 
-  getData()
+
+
 })
