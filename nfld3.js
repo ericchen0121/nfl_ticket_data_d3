b3ddf4df4d8947ebea8f6047be1d6d3f50254006
Eric Chen <eric.chen0121@gmail.com>
2014-09-18 17:49:54 -0700

    rotate axis to y

--- nfld3.js
+++ nfld3.js
@@ -5,8 +5,10 @@ $(function() {
     originX: 100,
     originY: 100,
     stepX: 70,
+    stepY: 70,
     animateStart: 3000,
-    animateInterval: 2000
+    animateInterval: 2000,
+    duration: 1500
   }
 
   var items
@@ -26,55 +28,105 @@ $(function() {
                         .attr('width', options.width)
                         .attr('height', options.height)
 
-  // the main draw function
-  function circleViz(data, key) {
-      // create a grouping
+  // this renders circles according to the key in the data
+  function drawCircles(data, key) {
       var circles = svg.selectAll('circle').data(data)
 
-      // update
+      // d3 update
       circles.transition()
-        .attr('cx', function(d, i){ return i * options.stepX + options.originX } )
-        .attr('cy', options.originY )
-        // the circle's size is the key
+        .attr('cx', options.originX + 150)
+        .attr('cy', function(d, i){ return i * options.stepY + options.originY } )
         // TODO: resize function to normalize the values
         .attr('r', function(d){ return d[key] / 4 } )
         .style('fill', 'black')
 
-      // exit
-      circles.exit().transition().duration(500).attr('r', 0).remove()
-
-      // enter
+      // d3 enter
       circles.enter().append('circle')
                         .attr('r', 0)
-                        .transition().duration(1500)
-                        .attr('cx', function(d, i){ return i * 70 + options.originX} )
-                        .attr('cy', options.originY )
+                        .transition().duration(options.duration)
+                        .attr('cy', function(d, i){ return i * 70 + options.originY} )
+                        .attr('cx', options.originX + 150)
                         .attr('r', function(d){ return d.avg_ticket / 4 } )
+                        // TODO: fill according to Hex value of team color
                         .style('fill', 'black')
+
+      // d3 exit
+      circles.exit().transition().duration(500).attr('r', 0).remove()
+
+
   }
 
-  function textViz(data, key) {
+  function drawLabels(data, key) {
     var text = svg.selectAll('text').data(data)
 
+    // update
     text.transition()
       .text( function(d) { return d[key] } )
-      .attr('x', function(d, i){ return i * options.stepX + options.originX } )
-      .attr('y', options.originY - 40)
+      .attr('x', options.originX - 40)
+      .attr('y', function(d, i){ return i * options.stepY + options.originY } )
 
+    // enter
     text.enter().append('text')
       .text( function(d) { return d[key] } )
       .attr('opacity', 0)
-      .transition().duration(1200)
+      .transition().duration(options.duration)
       .attr('opacity', 1)
-      .attr('x', function(d, i){ return i * options.stepX + options.originX } )
-      .attr('y', options.originY - 40)
+      .attr('x', options.originX - 40)
+      .attr('y', function(d, i){ return i * options.stepY + options.originY } )
 
+    // exit
     text.exit().transition().duration(500).attr('r', 0).remove()
 
   }
 
-  function imageViz(data) {
-    var images = svg.selectAll('img').data(data)
+  function drawLabels(data, key) {
+    var text = svg.selectAll('text').data(data)
+
+    // update
+    text.transition()
+      .text( function(d) { return d[key] } )
+      .attr('x', options.originX - 40)
+      .attr('y', function(d, i){ return i * options.stepY + options.originY } )
+
+    // enter
+    text.enter().append('text')
+      .text( function(d) { return d[key] } )
+      .attr('opacity', 0)
+      .transition().duration(options.duration)
+      .attr('opacity', 1)
+      .attr('x', options.originX - 40)
+      .attr('y', function(d, i){ return i * options.stepY + options.originY } )
+
+    // exit
+    text.exit().transition().duration(500).attr('r', 0).remove()
+
+  }
+
+  function addImage(data) {
+    for(i=0; i < data.length; i++)
+      data[i]['image'] = 'http://i.nflcdn.com/static/site/6.1/img/logos/svg/teams-matte-mascot/' + data[i]['team'].split(' ').pop() + '.svg';
+  }
+
+  // TODO: this also connects the image tag to the entire data set. Should you do this with just
+  function drawLogos(data, key) {
+    // implicit input here
+    var images = svg.selectAll('img').data(data, function(d) { return d[key] })
+
+    // update
+    images.transition()
+      .attr('href', function(d){ return d })
+      .attr('x', function(d, i){ return i * options.stepX + options.originX } )
+      .attr('y', options.originY - 80)
+
+    // enter
+    images.enter().append('img')
+      .attr('href', function(d){ return d })
+      .transition().duration(options.duration)
+      .attr('x', function(d, i){ return i * options.stepX + options.originX } )
+      .attr('y', options.originY - 40)
+
+    // exit
+    images.exit().transition().duration(500).attr('r', 0).remove()
 
     //update
     // images.transition()
@@ -92,83 +144,121 @@ $(function() {
   // @items[Array of Objects]
   // @key[String]
   // @filterList[array of key's values]
-  function filter(items, key, filterList) {
+  function filterData(data, key, filterList) {
     var resultsList = []
-    for(i = 0; i < items.length; i++){
-      // if the item's key exists in the filteredList
-      if(filterList.indexOf(items[i][key]) > -1) {
-        resultsList.push( items[i] )
+    for(i = 0; i < data.length; i++){
+      // if the data's key value exists in the filteredList
+      if(filterList.indexOf(data[i][key]) > -1) {
+        resultsList.push( data[i] )
       }
     }
 
     return resultsList
   }
 
-  function oneAtATime(items, interval) {
+  function oneAtATime(data, interval) {
     setInterval(function() {
-      items = items.splice(1, items.length - 1)
-      draw(items)
+      data = data.splice(1, data.length - 1)
+      draw(data)
     }, interval)
+  }
+
+  function draw(data) {
+    drawCircles(data, 'avg_ticket')
+    drawLabels(data, 'team')
+    // drawLogos(data, 'image')
+  }
+
+  function animate(data) {
+        // // TODO: create an animate function that calls the circle viz with animate
+    // setTimeout(function(){
+    //   drawCircles(filter(items, 'team', afc_north), 'avg_ticket')
+    // }, options.animateStart + options.animateInterval * 1)
+    // setTimeout(function(){
+    //   drawCircles(filter(items, 'team', afc_east), 'avg_ticket')
+    // }, options.animateStart + options.animateInterval * 2)
+    // setTimeout(function(){
+    //   drawCircles(filter(items, 'team', afc_south), 'avg_ticket')
+    // }, options.animateStart + options.animateInterval * 3)
+    // setTimeout(function(){
+    //   drawCircles(filter(items, 'team', afc_west), 'avg_ticket')
+    // }, options.animateStart + options.animateInterval * 4)
+    // setTimeout(function(){
+    //   drawCircles(filter(items, 'team', nfc_north), 'avg_ticket')
+    // }, options.animateStart + options.animateInterval * 5)
+    // setTimeout(function(){
+    //   drawCircles(filter(items, 'team', nfc_east), 'avg_ticket')
+    // }, options.animateStart + options.animateInterval * 6)
+    // setTimeout(function(){
+    //   drawCircles(filter(items, 'team', nfc_south), 'avg_ticket')
+    // }, options.animateStart + options.animateInterval * 7)
+    // setTimeout(function(){
+    //   drawCircles(filter(items, 'team', nfc_west), 'avg_ticket')
+    // }, options.animateStart + options.animateInterval * 8)
+
+    // drawLabels(items, 'team')
+    // // TODO: create an animate function that calls the circle viz with animate
+    // setTimeout(function(){
+    //   drawLabels(filter(items, 'team', afc_north), 'team')
+    // }, options.animateStart + options.animateInterval * 1)
+    // setTimeout(function(){
+    //   drawLabels(filter(items, 'team', afc_east), 'team')
+    // }, options.animateStart + options.animateInterval * 2)
+    // setTimeout(function(){
+    //   drawLabels(filter(items, 'team', afc_south), 'team')
+    // }, options.animateStart + options.animateInterval * 3)
+    // setTimeout(function(){
+    //   drawLabels(filter(items, 'team', afc_west), 'team')
+    // }, options.animateStart + options.animateInterval * 4)
+    // setTimeout(function(){
+    //   drawLabels(filter(items, 'team', nfc_north), 'team')
+    // }, options.animateStart + options.animateInterval * 5)
+    // setTimeout(function(){
+    //   drawLabels(filter(items, 'team', nfc_east), 'team')
+    // }, options.animateStart + options.animateInterval * 6)
+    // setTimeout(function(){
+    //   drawLabels(filter(items, 'team', nfc_south), 'team')
+    // }, options.animateStart + options.animateInterval * 7)
+    // setTimeout(function(){
+    //   drawLabels(filter(items, 'team', nfc_west), 'team')
+    // }, options.animateStart + options.animateInterval * 8)
+
+
+    // drawLogos(items, 'image')
+    // // TODO: create an animate function that calls the circle viz with animate
+    // setTimeout(function(){
+    //   drawLogos(filter(items, 'team', afc_north), 'team')
+    // }, options.animateStart + options.animateInterval * 1)
+    // setTimeout(function(){
+    //   drawLogos(filter(items, 'team', afc_east), 'team')
+    // }, options.animateStart + options.animateInterval * 2)
+    // setTimeout(function(){
+    //   drawLogos(filter(items, 'team', afc_south), 'team')
+    // }, options.animateStart + options.animateInterval * 3)
+    // setTimeout(function(){
+    //   drawLogos(filter(items, 'team', afc_west), 'team')
+    // }, options.animateStart + options.animateInterval * 4)
+    // setTimeout(function(){
+    //   drawLogos(filter(items, 'team', nfc_north), 'team')
+    // }, options.animateStart + options.animateInterval * 5)
+    // setTimeout(function(){
+    //   drawLogos(filter(items, 'team', nfc_east), 'team')
+    // }, options.animateStart + options.animateInterval * 6)
+    // setTimeout(function(){
+    //   drawLogos(filter(items, 'team', nfc_south), 'team')
+    // }, options.animateStart + options.animateInterval * 7)
+    // setTimeout(function(){
+    //   drawLogos(filter(items, 'team', nfc_west), 'team')
+    // }, options.animateStart + options.animateInterval * 8)
+
   }
 
   // get initial set of data and render it
   $.getJSON('nfl_ticket_data.json', function(data) {
-    items = data
-    circleViz(items, 'avg_ticket')
-    // TODO: create an animate function that calls the circle viz with animate
-    setTimeout(function(){
-      circleViz(filter(items, 'team', afc_north), 'avg_ticket')
-    }, options.animateStart + options.animateInterval * 1)
-    setTimeout(function(){
-      circleViz(filter(items, 'team', afc_east), 'avg_ticket')
-    }, options.animateStart + options.animateInterval * 2)
-    setTimeout(function(){
-      circleViz(filter(items, 'team', afc_south), 'avg_ticket')
-    }, options.animateStart + options.animateInterval * 3)
-    setTimeout(function(){
-      circleViz(filter(items, 'team', afc_west), 'avg_ticket')
-    }, options.animateStart + options.animateInterval * 4)
-    setTimeout(function(){
-      circleViz(filter(items, 'team', nfc_north), 'avg_ticket')
-    }, options.animateStart + options.animateInterval * 5)
-    setTimeout(function(){
-      circleViz(filter(items, 'team', nfc_east), 'avg_ticket')
-    }, options.animateStart + options.animateInterval * 6)
-    setTimeout(function(){
-      circleViz(filter(items, 'team', nfc_south), 'avg_ticket')
-    }, options.animateStart + options.animateInterval * 7)
-    setTimeout(function(){
-      circleViz(filter(items, 'team', nfc_west), 'avg_ticket')
-    }, options.animateStart + options.animateInterval * 8)
-
-    textViz(items, 'team')
-    // TODO: create an animate function that calls the circle viz with animate
-    setTimeout(function(){
-      textViz(filter(items, 'team', afc_north), 'team')
-    }, options.animateStart + options.animateInterval * 1)
-    setTimeout(function(){
-      textViz(filter(items, 'team', afc_east), 'team')
-    }, options.animateStart + options.animateInterval * 2)
-    setTimeout(function(){
-      textViz(filter(items, 'team', afc_south), 'team')
-    }, options.animateStart + options.animateInterval * 3)
-    setTimeout(function(){
-      textViz(filter(items, 'team', afc_west), 'team')
-    }, options.animateStart + options.animateInterval * 4)
-    setTimeout(function(){
-      textViz(filter(items, 'team', nfc_north), 'team')
-    }, options.animateStart + options.animateInterval * 5)
-    setTimeout(function(){
-      textViz(filter(items, 'team', nfc_east), 'team')
-    }, options.animateStart + options.animateInterval * 6)
-    setTimeout(function(){
-      textViz(filter(items, 'team', nfc_south), 'team')
-    }, options.animateStart + options.animateInterval * 7)
-    setTimeout(function(){
-      textViz(filter(items, 'team', nfc_west), 'team')
-    }, options.animateStart + options.animateInterval * 8)
+    draw(filterData(data, 'team', afc_north))
+    setTimeout(function(){draw(filterData(data, 'team', nfc_north))}, 2000)
+    // items = addImage(data)
+    // drawCircles(items, 'avg_ticket')
   })
-
-
 
 })
